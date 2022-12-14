123fddfbe3045a84ea3b0d358884ba5a37b993b1
MohamedYassineYAHYAOUI <yahyaouiy1997@gmail.com>
2022-12-14 16:44:50 +0100

    rename tests methods

--- src/test/java/fr/marsRover/BackwardCommandsTest.java
+++ src/test/java/fr/marsRover/BackwardCommandsTest.java
@@ -10,7 +10,7 @@ public class BackwardCommandsTest {
 
     @ParameterizedTest
     @CsvSource({"1,1,N,1,0,b", "0,1,N,0,0,b", "0,3,S,0,4,b", "6,4,S,6,5,b"})
-    public void should_return_correct_point_after_one_vertical_movement(int initialX, int initialY, char initialDirection,
+    public void should_return_correct_point_after_one_backward_movement(int initialX, int initialY, char initialDirection,
                                                                         int expectedX, int expectedY, String commands) {
         GridPoint initialPoint = new GridPoint(initialX, initialY);
         Rover rover = Rover.Create(initialPoint, initialDirection, testGrid);
@@ -20,17 +20,10 @@ public class BackwardCommandsTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,1,N,1,0,b", "0,1,N,0,0,b", "0,3,S,0,4,b", "6,4,S,6,5,b"})
-    public void should_return_correct_point_after_one_horizontal_movement(int initialX, int initialY, char initialDirection,
-                                                                          int expectedX, int expectedY, String commands) {
-        should_return_correct_point_after_one_vertical_movement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
-    }
-
-    @ParameterizedTest
     @CsvSource({"1,3,N,1,0,bbb", "1,3,S,1,5,bb", "4,4,E,0,4,bbbb", "3,3,W,6,3,bbb"})
     public void should_return_correct_point_after_multiple_backwards(int initialX, int initialY, char initialDirection
             , int expectedX, int expectedY, String commands) {
-        should_return_correct_point_after_one_vertical_movement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
+        should_return_correct_point_after_one_backward_movement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
     }
 
 }
--- src/test/java/fr/marsRover/ForwardCommandsTest.java
+++ src/test/java/fr/marsRover/ForwardCommandsTest.java
@@ -14,15 +14,15 @@ public class ForwardCommandsTest {
 
     @ParameterizedTest
     @CsvSource({"0,0,L", "0,0,*", "0,0,!", "-1,5,W", "1,-8,N",})
-    public void invalidInitialDirection(int initialX, int initialY, char initialDirection) {
-        GridPoint initialpoint = new GridPoint(initialX, initialY);
-        Assertions.assertThrows(InvalidParameterException.class, () -> Rover.Create(initialpoint, initialDirection, testGrid));
+    public void should_throw_on_invalid_direction_or_invalid_initial_point(int initialX, int initialY, char initialDirection) {
+        GridPoint initialPoint = new GridPoint(initialX, initialY);
+        Assertions.assertThrows(InvalidParameterException.class, () -> Rover.Create(initialPoint, initialDirection, testGrid));
     }
 
     @ParameterizedTest
     @CsvSource({"0,0,N,0,1,f", "0,1,N,0,2,f", "0,3,S,0,2,f", "6,4,S,6,3,f"})
-    public void singleYMovement(int initialX, int initialY, char initialDirection,
-                                int expectedX, int expectedY, String commands) {
+    public void should_return_correct_point_after_one_forward_vertical_movement(int initialX, int initialY, char initialDirection,
+                                                                                int expectedX, int expectedY, String commands) {
         GridPoint initialPoint = new GridPoint(initialX, initialY);
         Rover rover = Rover.Create(initialPoint, initialDirection, testGrid);
         rover.move(commands);
@@ -32,20 +32,20 @@ public class ForwardCommandsTest {
 
     @ParameterizedTest
     @CsvSource({"2,1,E,3,1,f", "5,2,W,4,2,f"})
-    public void singleXMovement(int initialX, int initialY, char initialDirection,
-                                int expectedX, int expectedY, String commands) {
-        singleYMovement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
+    public void should_return_correct_point_after_one_horizontal_forward_movement(int initialX, int initialY, char initialDirection,
+                                                                                  int expectedX, int expectedY, String commands) {
+        should_return_correct_point_after_one_forward_vertical_movement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
     }
 
     @ParameterizedTest
     @CsvSource({"1,3,N,1,6,fff", "1,3,S,1,1,ff", "1,3,E,5,3,ffff", "3,3,W,0,3,fff"})
-    public void multipleCommands(int initialX, int initialY, char initialDirection
+    public void should_return_correct_point_after_multiple_forward_commands(int initialX, int initialY, char initialDirection
             , int expectedX, int expectedY, String commands) {
-        singleYMovement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
+        should_return_correct_point_after_one_forward_vertical_movement(initialX, initialY, initialDirection, expectedX, expectedY, commands);
     }
 
     @Test
-    public void nullCommand() {
+    public void should_throw_on_null_command() {
         GridPoint initialPoint = new GridPoint(0, 0);
         Rover rover = Rover.Create(initialPoint, 'N', testGrid);
         Assertions.assertThrows(IllegalArgumentException.class, () -> rover.move(null));
