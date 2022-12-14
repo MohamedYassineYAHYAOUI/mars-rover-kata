70b738fd3e5d6eb08d68d6ad35bc1543ef8017b6
MohamedYassineYAHYAOUI <yahyaouiy1997@gmail.com>
2022-12-14 14:32:50 +0100

    refacto wrapping tests

--- src/test/java/fr/marsRover/WrappingEdgesTest.java
+++ src/test/java/fr/marsRover/WrappingEdgesTest.java
@@ -9,93 +9,56 @@ public class WrappingEdgesTest {
     private static final int TEST_GRID_HEIGHT = 10;
     private final Grid testGrid = new Grid(TEST_GRID_WIDTH, TEST_GRID_HEIGHT);
 
+    private void wrappingTest(int initialXPosition, int initialYPosition, char initialDirection, String commands,
+                              int expectedXPosition, int expectedYPosition, char expectedDirection) {
+        Rover rover = Rover.Create(new GridPosition(initialXPosition, initialYPosition), initialDirection, testGrid);
+
+        rover.move(commands);
+
+        Assertions.assertEquals(new GridPosition(expectedXPosition, expectedYPosition), rover.getPosition());
+        Assertions.assertEquals(expectedDirection, rover.getDirection());
+    }
+
     @Test
     public void wrappingWestToEastFacingWest() {
 
-        Rover rover = Rover.Create(new GridPosition(0, 0), 'N', testGrid);
-
-        rover.move("LF");
-
-        Assertions.assertEquals(new GridPosition(TEST_GRID_WIDTH, 0), rover.getPosition());
-        Assertions.assertEquals('W', rover.getDirection());
+        wrappingTest(0, 0, 'N', "LF", TEST_GRID_WIDTH, 0, 'W');
     }
 
 
     @Test
     public void wrappingWestToEastFacingEast() {
-        Rover rover = Rover.Create(new GridPosition(1, 5), 'N', testGrid);
-
-        rover.move("RBB");
-
-        Assertions.assertEquals(new GridPosition(TEST_GRID_WIDTH, 5), rover.getPosition());
-
-        Assertions.assertEquals('E', rover.getDirection());
+        wrappingTest(1, 5, 'N', "RBB", TEST_GRID_WIDTH, 5, 'E');
     }
 
     @Test
     public void wrappingEastToWestFacingEast() {
-        Rover rover = Rover.Create(new GridPosition(TEST_GRID_WIDTH, 0), 'N', testGrid);
-
-        rover.move("RF");
-
-        Assertions.assertEquals(new GridPosition(0, 0), rover.getPosition());
-
-        Assertions.assertEquals('E', rover.getDirection());
+        wrappingTest(TEST_GRID_WIDTH, 0, 'N', "RF", 0, 0, 'E');
     }
 
     @Test
     public void wrappingEastToWestFacingWest() {
-        Rover rover = Rover.Create(new GridPosition(7, 3), 'E', testGrid);
-
-        rover.move("RRBBBB");
-
-        Assertions.assertEquals(new GridPosition(0, 3), rover.getPosition());
-
-        Assertions.assertEquals('W', rover.getDirection());
+        wrappingTest(7, 3, 'E', "RRBBBB", 0, 3, 'W');
     }
 
     @Test
     public void wrappingNorthToSouthFacingNorth() {
-        Rover rover = Rover.Create(new GridPosition(7, 8), 'N', testGrid);
-
-        rover.move("FFFF");
-
-        Assertions.assertEquals(new GridPosition(7, 1), rover.getPosition());
-
-        Assertions.assertEquals('N', rover.getDirection());
+        wrappingTest(7, 8, 'N', "FFFF", 7, 1, 'N');
     }
 
     @Test
     public void wrappingNorthToSouthFacingSouth() {
-        Rover rover = Rover.Create(new GridPosition(7, 8), 'N', testGrid);
-
-        rover.move("FFLFLB");
-
-        Assertions.assertEquals(new GridPosition(6, 0), rover.getPosition());
-
-        Assertions.assertEquals('S', rover.getDirection());
+        wrappingTest(7, 8, 'N', "FFLFLB", 6, 0, 'S');
     }
 
     @Test
     public void wrappingSouthToNorthFacingSouth() {
-        Rover rover = Rover.Create(new GridPosition(6, 2), 'N', testGrid);
-
-        rover.move("RRFFF");
-
-        Assertions.assertEquals(new GridPosition(6, TEST_GRID_HEIGHT), rover.getPosition());
-
-        Assertions.assertEquals('S', rover.getDirection());
+        wrappingTest(6, 2, 'N', "RRFFF", 6, TEST_GRID_HEIGHT, 'S');
     }
 
     @Test
     public void wrappingSouthToNorthFacingNorth() {
-        Rover rover = Rover.Create(new GridPosition(5, 1), 'S', testGrid);
-
-        rover.move("RRBB");
-
-        Assertions.assertEquals(new GridPosition(5, TEST_GRID_HEIGHT), rover.getPosition());
-
-        Assertions.assertEquals('N', rover.getDirection());
+        wrappingTest(5, 1, 'S', "RRBB", 5, TEST_GRID_HEIGHT, 'N');
     }
 
 
