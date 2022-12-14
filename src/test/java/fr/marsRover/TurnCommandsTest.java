c831387979194b89853607945a7c8124804aa470
MohamedYassineYAHYAOUI <yahyaouiy1997@gmail.com>
2022-12-14 09:19:04 +0100

    refactor

--- src/test/java/fr/marsRover/TurnCommandsTest.java
+++ src/test/java/fr/marsRover/TurnCommandsTest.java
@@ -1,8 +1,6 @@
 package fr.marsRover;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
